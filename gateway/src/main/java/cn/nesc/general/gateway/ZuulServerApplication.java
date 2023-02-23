3010339dc1a1ceca135ef5d693c13b63c469e199
summer <sandrew1945@gmail.com>
2023-02-23 14:40:47 +0800

    BREAKING CHANGE: gateway依赖的优化
    1、优化gateway的依赖,不再单独引用依赖而统一使用framework指定
    
    Fixes

--- gateway/src/main/java/cn/nesc/general/gateway/ZuulServerApplication.java
+++ gateway/src/main/java/cn/nesc/general/gateway/ZuulServerApplication.java
@@ -5,13 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
 @EnableFeignClients(basePackages = {"cn.nesc.general.stub.authcenter"})
 @EnableZuulProxy
 @EnableCaching
